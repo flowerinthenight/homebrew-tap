be457ea45e1ff7e52ec1dc60bd876c2afee23bba
goreleaserbot <goreleaser@carlosbecker.com>
2020-07-15 18:36:12 +0900

    Brew formula update for oops version v0.3.1

--- Formula/oops.rb
+++ Formula/oops.rb
@@ -2,16 +2,16 @@
 class Oops < Formula
   desc "Kubernetes-native testing tool."
   homepage "https://github.com/flowerinthenight/oops"
-  version "0.3.0"
+  version "0.3.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/flowerinthenight/oops/releases/download/v0.3.0/oops_0.3.0_darwin_amd64.tar.gz"
-    sha256 "b92ee57c9f93e0426fc00b04771c702715e34271ab695fde597695610b808547"
+    url "https://github.com/flowerinthenight/oops/releases/download/v0.3.1/oops_0.3.1_darwin_amd64.tar.gz"
+    sha256 "ecdd84fa4b4531ca2bbd2e6c7cb708b3fa4ef70cafac19c808200d109b3e241e"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/flowerinthenight/oops/releases/download/v0.3.0/oops_0.3.0_linux_amd64.tar.gz"
-      sha256 "a2a60310cf0fec79b359a298bb3a19df15fb9de0a0d25d55471723093517d1c1"
+      url "https://github.com/flowerinthenight/oops/releases/download/v0.3.1/oops_0.3.1_linux_amd64.tar.gz"
+      sha256 "52860ff90054a5d789f988a238005f8203b6ee1611106c1c781a2a9a3775252d"
     end
   end
 
