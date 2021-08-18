f0ee3f26ea752b8aa4543b95fc20e6030e30c578
goreleaserbot <goreleaser@carlosbecker.com>
2021-08-18 14:49:06 +0900

    Brew formula update for kubepfm version v1.5.2

--- Formula/kubepfm.rb
+++ Formula/kubepfm.rb
@@ -5,16 +5,21 @@
 class Kubepfm < Formula
   desc "Simple port-forward wrapper tool for multiple pods."
   homepage "https://github.com/flowerinthenight/kubepfm"
-  version "1.5.1"
+  version "1.5.2"
   bottle :unneeded
 
-  if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/flowerinthenight/kubepfm/releases/download/v1.5.1/kubepfm_1.5.1_darwin_amd64.tar.gz"
-    sha256 "4c495b586633312273d8b81403424fad71bf3b0d5f73fbeaf774a8f476ad77fc"
+  on_macos do
+    if Hardware::CPU.intel?
+      url "https://github.com/flowerinthenight/kubepfm/releases/download/v1.5.2/kubepfm_1.5.2_darwin_amd64.tar.gz"
+      sha256 "91b00ad39dc4eb2e7afae4628ca9a9b2d8117443d6edb6cffe0eb1eec917de05"
+    end
   end
-  if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/flowerinthenight/kubepfm/releases/download/v1.5.1/kubepfm_1.5.1_linux_amd64.tar.gz"
-    sha256 "ad65e443d21090c63328f5bb0ece69fd433fd9c372a91a552c878fc1b4828ace"
+
+  on_linux do
+    if Hardware::CPU.intel?
+      url "https://github.com/flowerinthenight/kubepfm/releases/download/v1.5.2/kubepfm_1.5.2_linux_amd64.tar.gz"
+      sha256 "b602740b1109daecb9e347201063f3f5555f73e13d8a54d8f176e3cc9722fc98"
+    end
   end
 
   def install
